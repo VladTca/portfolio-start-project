97c836606122e9d205bdf6f65ab7606f0e4d82b2
VladTca <votsats@gmail.com>
2024-03-28 04:26:54 +0100

    feat(layout): Add Main section to the layout
    - Added Main.tsx file to src/layout/sections/main
    - Created Main component with basic structure and styling
    - Added FlexWrapper component for flexible layout styling

---
+++ src/layout/sections/main/Main.tsx
@@ -0,0 +1,38 @@
+import React from "react";
+import styled from "styled-components";
+import photo from "../../../assets/images/sveta.webp";
+import { FlexWrapper } from "../../../components/FlexWrapper";
+
+export const Main = () => {
+  return (
+    <StyledMain>
+      <FlexWrapper align={"center"} justify={"space-around"} direction={"row"}>
+        <div>
+          <span>Hi There</span>
+          <NameSD>I am Svetlana Dyablo</NameSD>
+          <MainTitle>A Web Developer. </MainTitle>
+        </div>
+        <Photo src={photo} />
+      </FlexWrapper>
+    </StyledMain>
+  );
+};
+
+const StyledMain = styled.div`
+  background-color: bisque;
+  min-height: 100vh;
+`;
+
+const Photo = styled.img`
+  width: 350px;
+  height: 430px;
+  object-fit: cover;
+`;
+
+const MainTitle = styled.h1`
+  font-size: 60px;
+`;
+
+const NameSD = styled.h2`
+  font-size: 60px;
+`;
