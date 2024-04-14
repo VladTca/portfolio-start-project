ff95309676dca2458921fd23c7ebf1dbcc621759
VladTca <votsats@gmail.com>
2024-04-14 16:44:37 +0200

    "Add cursor pointer style to anchor tags in Global.styled.ts and import theme in Footer.tsx"

--- src/layout/footer/Footer.tsx
+++ src/layout/footer/Footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import styled from "styled-components";
 import { Icon } from "../../components/icon/Icon";
 import { FlexWrapper } from "../../components/FlexWrapper";
+import {theme} from "../../styles/Theme";
 
 export const Footer = () => {
   return (
@@ -57,13 +58,51 @@ export const Footer = () => {
 };
 
 const StyledFooter = styled.footer`
-  min-height: 20vh;
+padding:40px 0;
+    background-color: ${theme.colors.primaryBg};
 `;
-const Name = styled.span``;
+const Name = styled.span`
+  
+  font-family: "Josefin Sans", sans-serif;
+  font-size: 22px;
+  font-weight: 700;
+  letter-spacing: 3px;
+
+`;
+
 const SocialList = styled.ul`
   display: flex;
-  gap: 30px;
+  gap: 20px;
+  margin: 30px 0;
 `;
+
+
+
 const SocialItem = styled.li``;
-const SocialLink = styled.a``;
-const Copyright = styled.small``;
+const SocialLink = styled.a`
+  border-radius: 50%;
+  background-color: rgba(255, 255, 255, 0.10);
+width: 35px;
+    height: 35px;
+  display: flex;
+  justify-content: center;
+    align-items: center;
+  
+  color:${theme.colors.accent};
+  
+    &:hover {
+        color: ${theme.colors.primaryBg};
+      transform: translateY(-4px);
+    }
+`;
+
+const Copyright = styled.small`
+  
+  text-align: center;
+opacity: 0.5;
+  font-size: 12px;
+  
+  font-weight: 400;
+  
+
+`;
