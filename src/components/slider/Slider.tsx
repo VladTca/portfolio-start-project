2189be9cdbe35604f6cd6268da024818afabf052
VladTca <votsats@gmail.com>
2024-04-21 21:50:44 +0200

    Refactor slider styles and add new CSS file for slider pagination styling

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/alice-carousel.css";
 import { S } from "./Slider_Styles";
+import "./../../styles/Slider.css";
 
 type SlidePropsType = {
   text: string;
@@ -43,29 +44,3 @@ export const Slider = () => (
     <AliceCarousel mouseTracking items={items} />
   </S.Slider>
 );
-
-// import React from 'react';
-// import {FlexWrapper} from "../FlexWrapper";
-// import {S} from "./Slider_Styles";
-//
-//
-// export const Slider:React.FC = () => {
-//     return (
-//         <S.Slider>
-//             <FlexWrapper>
-//
-//                 <S.Slide>
-//                     <S.Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-//                         labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-//                         elit.</S.Text>
-//                     <S.Name>@ivan ivanow</S.Name>
-//                 </S.Slide>
-//             </FlexWrapper>
-//             <S.Pagination>
-//                 <span></span>
-//                 <span className={"active"}></span>
-//                 <span></span>
-//             </S.Pagination>
-//         </S.Slider>
-//     );
-// };
